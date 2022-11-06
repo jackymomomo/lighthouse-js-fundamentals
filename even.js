3d9a9709e7a1addf98598093ec356e1de91c6807
jackymomomo <jacklloydrobinson@gmail.com>
2022-11-06 01:54:14 -0800

    basic function calls

---
+++ even.js
@@ -0,0 +1,10 @@
+const isEven = num => {
+  return num % 2 === 0;
+}
+
+// not needed if pass peramitar in console.log() such as isEven(10)
+// const isTen = isEven(10)
+// const isEleven = isEven(11)
+
+console.log(isEven(10))
+console.log(isEven(11))
---
+++ hello.js
@@ -0,0 +1,24 @@
+// const sayHello = (name) => {
+//   console.log("Hello, " + name);
+// }
+
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+
+// console.log
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+// return
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+
