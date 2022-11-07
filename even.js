85cbb4a5626bd39297d7e0a9fe840c89129d5e6b
jackymomomo <jacklloydrobinson@gmail.com>
2022-11-06 17:23:12 -0800

    updated to while loop

--- even.js
+++ even.js
@@ -1,11 +1,11 @@
-const isEven = num => {
-  return num % 2 === 0;
-  return num %  === 0;
-}
+// const isEven = num => {
+//   return num % 2 === 0;
+//   return num %  === 0;
+// }
 
-// not needed if pass peramitar in console.log() such as isEven(10)
-// const isTen = isEven(10)
-// const isEleven = isEven(11)
+// // not needed if pass peramitar in console.log() such as isEven(10)
+// // const isTen = isEven(10)
+// // const isEleven = isEven(11)
 
-console.log(isEven(10))
-console.log(isEven(11))
+// console.log(isEven(10))
+// console.log(isEven(11))
